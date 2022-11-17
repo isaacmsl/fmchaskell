01dc311b6433767f4e0b52c8b7770c212c909b0b
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2022-11-17 17:35:00 -0300

    initial commit

---
+++ IRI.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE GADTs, EmptyDataDecls #-}
+
+module IRI where
+
+data Nat where
+    O :: Nat
+    S :: Nat -> Nat
+  deriving (Eq, Show) 
+
+data Unit where
+    Star :: Unit
+
+data Empty
+
+data List a where
+    Nil  :: List a
+    Cons :: a -> List a -> List a
+  deriving (Eq, Show)
+
+len :: [a] -> Nat
+len [] = O
+len (x : xs) = S (len xs)
